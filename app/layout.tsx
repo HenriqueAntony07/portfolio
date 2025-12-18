7bc4f23ddd707bd06417bd537591ddb708df6ef5
Henrique Antony <henriqueantonydev@gmail.com>
2025-12-18 11:23:18 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { JetBrains_Mono } from "next/font/google"
-import "globals.css"
+import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 
 const jetbrainsMono = JetBrains_Mono({
