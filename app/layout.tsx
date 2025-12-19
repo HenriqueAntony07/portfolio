3bd1b7ccadaa94f6c7de3eac5ef08b0c142edfc3
Henrique Antony <henriqueantonydev@gmail.com>
2025-12-19 11:28:48 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -122,7 +122,7 @@ export default function RootLayout({
           src="https://www.googletagmanager.com/gtag/js?id=G-6PH3GS6DSK"
           strategy="afterInteractive"
         />
-        <Script id="ga4-init" strategy="afterInteractive">
+        <Script id="ga4-init" strategy="lazyOnload">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
