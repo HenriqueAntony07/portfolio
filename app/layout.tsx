be62f13d6ccf6812b2dcf50214c97a37f20b68c3
Henrique Antony <henriqueantonydev@gmail.com>
2025-12-30 09:53:59 -0300

    commit
    metapixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -219,6 +219,25 @@ export default function RootLayout({
             })();`,
           }}
         />
+        
+        <!-- Meta Pixel Code -->
+        <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1524983748727453');
+        fbq('track', 'PageView');
+        </script>
+        <noscript><img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=1524983748727453&ev=PageView&noscript=1"
+        /></noscript>
+        <!-- End Meta Pixel Code -->
+
       </head>
       <body className={`${geist.className} ${geist.variable}`}>
         <ThemeProvider>{children}</ThemeProvider>
