cfe574ee32d7c0d38a1b90a8174ad93894a3e6b6
Henrique Antony <henriqueantonydev@gmail.com>
2026-02-13 15:22:58 -0300

    commit
    ajuste loader

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -3,30 +3,22 @@
 import { useEffect, useState } from "react";
 import styles from "./Loader.module.css";
 
-
 export default function Loader() {
-  const [visible, setVisible] = useState(true);
+  const [hide, setHide] = useState(false);
 
   useEffect(() => {
-    const handleLoad = () => {
-      setTimeout(() => {
-        setVisible(false);
-      }, 800);
-    };
+    const timer = setTimeout(() => {
+      setHide(true);
+    }, 1800); // tempo total do loader
 
-    if (document.readyState === "complete") {
-      handleLoad();
-    } else {
-      window.addEventListener("load", handleLoad);
-      return () => window.removeEventListener("load", handleLoad);
-    }
+    return () => clearTimeout(timer);
   }, []);
 
-  if (!visible) return null;
-
   return (
-    <div className={styles.loader}>
-        <span className={styles.text}>Henrique Antony</span>
+    <div className={`${styles.loader} ${hide ? styles.hide : ""}`}>
+      <div className={styles.inner}>
+        <h1 className={styles.logo}>Henrique Antony</h1>
+      </div>
     </div>
   );
 }
